1e94a07b6bab8c6820216815407f62d41e5f635d
Gabriel Marghoti <gabrielmarghoti@gmail.com>
2025-02-18 23:33:12 +0100

    organizing files and rename

---
+++ nonlinfunconn/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+
+class CustomBuildExtCommand(build_ext):
+    """Custom build_ext command to include numpy headers dynamically."""
+
+    def build_extensions(self):
+        import numpy as np  # Import numpy only when required
+        for ext in self.extensions:
+            ext.include_dirs.append(np.get_include())  # Append numpy headers
+        super().build_extensions()
+
+# Define the C++ extensions (without hardcoded numpy include)
+_integration = Extension(
+    name='utils._integration',
+    sources=['utils/_integration.cpp'],
+    extra_compile_args=['-O3']  # Optimization flag
+)
+
+_convolution = Extension(
+    name='utils._convolution',
+    sources=['utils/_convolution.cpp', 'utils/convolution.cpp'],
+    extra_compile_args=['-O3']  # Optimization flag
+)
+
+setup(
+    name='nonlinfunconn',
+    version='0.1',
+    packages=find_packages(),  # Automatically find packages
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+    ext_modules=[_integration, _convolution],  # List of C++ extensions
+    cmdclass={'build_ext': CustomBuildExtCommand},  # Use the custom build_ext command
+    author='Gabriel Marghoti',
+    author_email='gabrielmarghoti@gmail.com',
+    description='Convolution kernel fitting for complex systems dynamics.',
+    url='https://github.com/GabrielMarghoti/nonlinfunconn',
+)
--- scripts/SIS_simulation_fit.sh
+++ scripts/SIS_simulation_fit.sh
@@ -3,4 +3,4 @@
 python3 SIS\ model/SIS_simulation.py --plot_data
 
 # Fit linear exponential kernels for functional connectivity as in "Neural signal propagation atlas of Caenorhabditis elegans 2023"
-python3 fit_exp_kernel.py --plot_data --data_folder SIS\ model/trials_data/
+python3 scripts/fit_exp_kernel.py --plot_data --data_folder SIS\ model/trials_data/

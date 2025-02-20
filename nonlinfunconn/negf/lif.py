aca08375681abe463766b9c42ce96b6e8ae5aa42
Gabriel Marghoti <gabrielmarghoti@gmail.com>
2025-02-21 00:28:19 +0100

    lif NEGF improvement

--- nonlinfunconn/negf/lif.py
+++ nonlinfunconn/negf/lif.py
@@ -1,10 +1,35 @@
+
 import numpy as np
 
+from nonlinfunconn import convolution
+
 class LIF:
-    def __init__(self, num_neurons, resolution, time_array, Veq, Seq, Vs, delta_Vs, delta_Ss, gamma_g, gamma_s, gamma, beta, V_th, Es, a_r, a_d):
+    def __init__(self, num_neurons, dt, Veq, Seq, Vs, delta_Vs, delta_Ss, gamma_g, gamma_s, gamma, beta, V_th, Es, a_r, a_d):
+        """
+        Initialize the LIF (Leaky Integrate-and-Fire) model.
+        The NEGF of LIF network requires as entry: the model parameters, nodes states and initial conditions (typically assumed as the equilibrium) variable values.
+
+        Parameters:
+            num_neurons (int): Number of neurons.
+            dt: Time step.
+            Veq (float or np.ndarray): Equilibrium membrane potentials.
+            Seq (float or np.ndarray): Equilibrium synaptic states.
+            Vs (np.ndarray): Membrane potentials over time.
+            delta_Vs (np.ndarray): Deviation from equilibrium membrane potentials.
+            delta_Ss (np.ndarray): Deviation from equilibrium synaptic states.
+            gamma_g (float or np.ndarray): Gap junction coupling strengths.
+            gamma_s (float or np.ndarray): Chemical synapse coupling strengths.
+            gamma (float or np.ndarray): Leakage rates.
+            beta (float or np.ndarray): Synaptic activation steepness.
+            V_th (float or np.ndarray): Threshold potentials.
+            Es (float or np.ndarray): Synaptic reversal potentials.
+            a_r (float or np.ndarray): Synaptic rise rates.
+            a_d (float or np.ndarray): Synaptic decay rates.
+        """
+
         self.num_neurons = num_neurons
-        self.resolution = resolution
-        self.time_array = time_array
+        self.dt = dt
+        self.resolution = Vs.shape[1]
 
         # Expand scalar parameters to arrays if necessary
         self.Veq = self._expand_to_array(Veq, num_neurons)
@@ -22,13 +47,13 @@ class LIF:
         self.a_d = self._expand_to_array(a_d, (num_neurons, num_neurons))
 
         # Initialize Green function arrays
-        self.sigma_0 = np.zeros((resolution, resolution, num_neurons, num_neurons))
-        self.gg_0 = np.zeros((resolution, resolution, num_neurons, num_neurons))
-        self.gs_0 = np.zeros((resolution, resolution, num_neurons, num_neurons))
-        self.g_0 = np.zeros((resolution, resolution, num_neurons, num_neurons))
-        self.sigma = np.zeros((resolution, resolution, num_neurons, num_neurons))
-        self.pi = np.zeros((resolution, resolution, num_neurons, num_neurons))
-        self.g = np.zeros((resolution, resolution, num_neurons, num_neurons))
+        self.sigma_0 = np.zeros((self.resolution, self.resolution, num_neurons, num_neurons))
+        self.gg_0 = np.zeros((self.resolution, self.resolution, num_neurons, num_neurons))
+        self.gs_0 = np.zeros((self.resolution, self.resolution, num_neurons, num_neurons))
+        self.g_0 = np.zeros((self.resolution, self.resolution, num_neurons, num_neurons))
+        self.sigma = np.zeros((self.resolution, self.resolution, num_neurons, num_neurons))
+        self.pi = np.zeros((self.resolution, self.resolution, num_neurons, num_neurons))
+        self.g = np.zeros((self.resolution, self.resolution, num_neurons, num_neurons))
 
     def _expand_to_array(self, value, shape):
         if np.isscalar(value):
@@ -51,10 +76,6 @@ class LIF:
         exp_term = np.exp(-beta * (V - V_th))
         return (beta * exp_term) / (1 + exp_term) ** 2
 
-    def convolution(self, X, Y, interval):
-        dt = (interval[-1] - interval[0]) / len(interval)
-        return dt * np.sum(X * Y, axis=0)
-
     def compute_equilibrium_green_functions(self):
         time_diff = self.time_array[:, None] - self.time_array
         heaviside_diff = self.heaviside(time_diff)
@@ -71,7 +92,7 @@ class LIF:
 
                 for t in range(self.resolution):
                     for t_prime in range(t):
-                        self.g_0[t, t_prime, i, j] = self.gg_0[t, t_prime, i, j] + self.convolution(self.gs_0[t, t_prime:t, i, j], self.sigma_0[t_prime:t, t_prime, i, j], self.time_array[t_prime:t])
+                        self.g_0[t, t_prime, i, j] = self.gg_0[t, t_prime, i, j] + convolution(self.gs_0[t, t_prime:t, i, j], self.sigma_0[t_prime:t, t_prime, i, j], self.dt, 8)
 
     def compute_nonequilibrium_green_functions(self):
         conv_sigma_V = np.zeros((self.resolution, self.num_neurons, self.num_neurons))
@@ -87,8 +108,8 @@ class LIF:
                                 synaptic_diff = (self.synaptic_activation(self.Vs[t_prime, j], self.beta[i, j], self.V_th[i, j]) - self.synaptic_activation(self.Veq[j], self.beta[i, j], self.V_th[i, j])) / self.delta_Vs[t_prime, j]
                                 self.sigma[t, t_prime, i, j] = self.sigma_0[t, t_prime, i, j] / self.d_synaptic_activation(self.Veq[j], self.beta[i, j], self.V_th[i, j]) * synaptic_diff * (1 - (conv_sigma_V[t_prime, i, j] / (1 - self.Seq[i, j])))
 
-                        conv_sigma_V[t, i, j] = self.convolution(self.sigma[t, :t, i, j], self.delta_Vs[:t, j], self.time_array[:t])
+                        conv_sigma_V[t, i, j] = convolution(self.sigma[t, :t, i, j], self.delta_Vs[:t, j], self.dt, 8)
 
                         for t_prime in range(t):
-                            self.pi[t, t_prime, i, j] = self.convolution(self.gs_0[t, t_prime:t, i, j], (1 - (self.delta_Vs[t_prime:t, i] / (self.Es[i, j] - self.Veq[i]))) * self.sigma[t_prime:t, t_prime, i, j], self.time_array[t_prime:t])
+                            self.pi[t, t_prime, i, j] = convolution(self.gs_0[t, t_prime:t, i, j], (1 - (self.delta_Vs[t_prime:t, i] / (self.Es[i, j] - self.Veq[i]))) * self.sigma[t_prime:t, t_prime, i, j], self.dt, 8)
                             self.g[t, t_prime, i, j] = self.gg_0[t, t_prime, i, j] + self.pi[t, t_prime, i, j]

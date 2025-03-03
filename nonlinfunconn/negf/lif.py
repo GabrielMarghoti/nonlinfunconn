4a1841dc333f0239043ce678368b40271064e203
Gabriel Marghoti <gabrielmarghoti@gmail.com>
2025-03-03 10:11:12 +0100

    fit negf.lif

--- nonlinfunconn/negf/lif.py
+++ nonlinfunconn/negf/lif.py
@@ -114,8 +114,12 @@ class LIF:
                             self.pi[t, t_prime, i, j] = convolution(self.gs_0[t, t_prime:t, i, j], (1 - (self.delta_Vs[t_prime:t, i] / (self.Es[i, j] - self.Veq[i]))) * self.sigma[t_prime:t, t_prime, i, j], self.dt, 8)
                             self.g[t, t_prime, i, j] = self.gg_0[t, t_prime, i, j] + self.pi[t, t_prime, i, j]
 
-    def fit(signal, dt):
-        """
+    def fit(cls,x,dt,
+            n_neigh_max=2,
+            rms_limits=[None,None],auto_stop=False,rms_tol=1e-2,
+            method=None,routine="least_squares"):
+        
+                """
         Fit the LIF model to the given signal for the entire network at once.
 
         Parameters:
@@ -150,4 +154,88 @@ class LIF:
         lif.compute_equilibrium_green_functions()
         lif.compute_nonequilibrium_green_functions()
 
-        return lif
+
+        from scipy.optimize import root,minimize,least_squares
+        rms = []
+        
+        y_norm = np.sum(y)
+        if np.isnan(y_norm) or np.isinf(y_norm) or y_norm==0:
+            return None, None, None
+        else:
+            yb = y/y_norm
+        #yb = y/y_norm
+        stim_norm = np.sum(stim)
+        stimb = stim/stim_norm
+        
+        p0_tot_prev_ = np.array([])
+        p_prev = np.array([])
+        n_in_prev = np.array([],dtype=int)
+                
+        for n_branches in np.arange(0,n_branches_max):
+            
+            rms_cur_b = []
+            for i in np.arange(n_hops_min,n_hops_max+1):
+                p0_cur_b_ = 0.2+np.arange(i)*0.03+n_branches*0.02
+                if n_branches == 0:
+                    A0 = 1.
+                else:
+                    A0 = (-1)**n_branches
+                p0_cur_b_ = np.append(A0,p0_cur_b_)
+                
+                p0_tot_ = np.append(p0_tot_prev_,p0_cur_b_)
+                p0_tot = mf.struct.irrarray(p0_tot_,[np.append(n_in_prev,i+1)],["branch"])
+                
+                lower_bounds = -np.inf*np.ones_like(p0_tot)
+                upper_bounds = np.inf*np.ones_like(lower_bounds)
+                special_idx = np.append(0,np.cumsum(n_in_prev))
+                for q in np.arange(len(lower_bounds)):
+                    if q not in special_idx:
+                        lower_bounds[q] = 0.0
+                    else:
+                        branch_idx = np.where(special_idx==q)[0][0]
+                        #if branch_idx%2==1: upper_bounds[q]=0.0
+                        #elif branch_idx%2==0 and n_branches>0: lower_bounds[q]=0.0
+                        
+                if routine == "minimize":
+                    error = lambda p,x,y: np.sum(np.power(pp.convolution(stimb,cls.eci(x,p),dt,8)-y,2))
+                    res = minimize(error,p0_tot,args=(x,yb),method=method)
+                    p_cur_b = res.x
+                elif routine == "least_squares":
+                    residuals = lambda p,x,y: pp.convolution(stimb,cls.eci(x,p),dt,8)-y
+                    #residuals = cls.residuals_branching_least_squares
+                    res = least_squares(residuals,p0_tot,args=(x,yb),method=method,bounds=(lower_bounds,upper_bounds))
+                    p_cur_b = res.x
+                rms_cur_b.append(np.sqrt(np.sum(np.power((cls.eci(x,p_cur_b)-yb)[rms_limits[0]:rms_limits[1]],2))))
+                
+                if auto_stop and i>n_hops_min:
+                    delta_rms_rel = np.abs(rms_cur_b[-1]-rms_cur_b[-2])/rms_cur_b[-2]
+                    if delta_rms_rel<rms_tol:break
+
+            p0_tot_prev_= p0_tot_
+            rms.append(rms_cur_b[-1])
+            
+            if auto_stop and n_branches>0:
+                delta_rms_rel = (rms[-1]-rms[-2])/rms[-2]
+                if np.abs(delta_rms_rel)<rms_tol and delta_rms_rel<0:
+                    n_in_prev = np.append(n_in_prev,i+1)
+                    p_prev = p_cur_b
+                    break
+                elif np.abs(delta_rms_rel)<rms_tol and delta_rms_rel>=0: 
+                    rms.pop(-1)
+                    break
+                else:
+                    n_in_prev = np.append(n_in_prev,i+1)
+                    p_prev = p_cur_b
+            else:
+                n_in_prev = np.append(n_in_prev,i+1)
+                p_prev = p_cur_b
+        
+        special_idx = np.append(0,np.cumsum(n_in_prev))
+        for j in special_idx[:-1]:
+            #FIXME BE CAREFUL WITH THIS. IF YOU HAVE REJOINING OF BRANCHES, IT
+            # IS NOT GOING TO BEHAVE WELL!
+            p_prev[j] *= y_norm/stim_norm
+            
+        gc.collect()
+            
+        return p_prev,n_in_prev,rms

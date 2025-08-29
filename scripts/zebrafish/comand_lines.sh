3b3c344bb7bb0b72cca5e242e7416edf502af211
Gabriel Marghoti <gabrielmarghoti@gmail.com>
2025-08-29 08:41:44 -0300

    .

--- scripts/zebrafish/comand_lines.sh
+++ scripts/zebrafish/comand_lines.sh
@@ -3,4 +3,4 @@
 .venv/bin/python3.10 scripts/zebrafish/open_data.py  --load-cache --plot
 
 # fit the neural mass model to the stimulated activity data
-.venv/bin/python3.10 scripts/zebrafish/fit_NMM.py --plot
+.venv/bin/python3.10 scripts/zebrafish/fit_NMM.py --plot --load-cache

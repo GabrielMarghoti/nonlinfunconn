overflow bug correction correction

nonlinfunconn/models/nm.py
@@ -109,7 +109,7 @@ class NM:
 
         self.ts = np.arange(0, self.time_len * self.dt, self.dt)
         ts_diff =  self.ts[:, np.newaxis] - self.ts # (time_len, time_len)
-        ts_diff = np.maximum(ts_diff, 0)
+        ts_diff = np.minimum(ts_diff, 0)
         heaviside_func = self.heaviside(ts_diff)
 
         # SHAPE: (num_nodes, num_nodes, time_len, time_len)

scripts/zebrafish/fit_NMM.py
@@ -30,7 +30,7 @@ spondata_tag = "20250625_5_1_spondata.mat"
 
 #regions to analyze
 
-regions = np.arange(53) # np.array([34, 35, 36, 44, 45]) # np.array([2, 4, 6, 8, 10, 16, 20, 28, 30, 34, 35, 36, 44, 45, 50])
+regions = np.array([34, 35, 36, 44, 45]) #p.arange(53)  # np.array([2, 4, 6, 8, 10, 16, 20, 28, 30, 34, 35, 36, 44, 45, 50])
 
 n_regions = regions.shape[0]
 
@@ -40,7 +40,7 @@ dt = 0.3
 
 fitting_window = 120
 
-lowering_fit_resolution_step = 6
+lowering_fit_resolution_step = 2
 
 file_path = os.path.join(data_dir, stimdata_tag)
 
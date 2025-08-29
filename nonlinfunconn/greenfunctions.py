overflow bug correctd

nonlinfunconn/greenfunctions.py
@@ -373,7 +373,7 @@ class GreenFunctions:
                 # Apply constraints if provided
                 if constrain is not None and key in constrain[0]:
                     p[key] = np.clip(p[key], constrain[0][key], constrain[1][key])
-                if not self_connection and key == "w":
+                if (not self_connection) and (key == "w"):
                     np.fill_diagonal(p[key], 0)
 
             current_loss = loss(self, p, x, target)

nonlinfunconn/models/nm.py
@@ -109,7 +109,7 @@ class NM:
 
         self.ts = np.arange(0, self.time_len * self.dt, self.dt)
         ts_diff =  self.ts[:, np.newaxis] - self.ts # (time_len, time_len)
-
+        ts_diff = np.maximum(ts_diff, 0)
         heaviside_func = self.heaviside(ts_diff)
 
         # SHAPE: (num_nodes, num_nodes, time_len, time_len)
@@ -117,14 +117,13 @@ class NM:
 
         g = np.zeros(green_shape)
 
-
         for i in range(num_nodes):
             for j in range(num_nodes):
                 if self.w[i, j] == 0:
                     continue  # Skip non-connected neurons
                 
                 small_delta_mask = np.abs(delta_xs[j]) >= 0.0001
-
+                
                 # Compute Green's function as before
                 g[i, j][:, small_delta_mask] = heaviside_func[:, small_delta_mask] * np.exp(-ts_diff[:, small_delta_mask] / self.tau[i]) * self.w[i, j] * (
                     (self.phi(xs[j, small_delta_mask], self.beta[i, j], self.xth[i, j]) - self.phi(xs[j, 0], self.beta[i, j], self.xth[i, j]))

scripts/zebrafish/fit_NMM.py
@@ -112,7 +112,7 @@ nm_kernels = nlfc.GreenFunctions(
 
 print("NEGF fitting")
 min_constrain_dict = {
-                    "tau": 0.0001  ,             
+                    "tau": 0.00001  ,             
                     "w": -1000,                   # Synaptic weight
                     "beta": 0.00001,                # Steepness of the sigmoid function
                     "xth": -100.0,                  # Threshold potential for synapse activation
@@ -144,14 +144,6 @@ fitted_parameters = nm_kernels.ADAM_fit(
 
 print('FITTING DONE')
 print("fitted parameters: ", fitted_parameters['w'])
-
-# Set main diagonal of fitted_parameters['w'] to 0
-if isinstance(fitted_parameters['w'], np.ndarray):
-    np.fill_diagonal(fitted_parameters['w'], 0)
-elif isinstance(fitted_parameters['w'], list):
-    w_array = np.array(fitted_parameters['w'])
-    np.fill_diagonal(w_array, 0)
-    fitted_parameters['w'] = w_array
 
 # Save parameters after fitting as a tab-delimited text file
 with open(os.path.join(output_data_dir, f"fitted_parameters_{regions}.txt"), "w") as f: